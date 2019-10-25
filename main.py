d2e5aa376e13b2170a857e5a8bd2cc1c586b01bd
Almeswe <52577119+Almeswe@users.noreply.github.com>
2019-10-26 01:13:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from NewCardGame.functions import card_balance,card_balanceBOT,events,update_window
+from NewCardGame.functions import events,update_window
 import pygame
 
 pygame.init()
-
-card_balance()
-card_balanceBOT()
 
 def Durak():
     while True:
